3a5004e8f0c1930d106f44a5add6d002bd505447
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-17 23:36:19 +0700

    Update companies log model

--- app/Models/Companies.php
+++ app/Models/Companies.php
@@ -2,24 +2,26 @@
 
 namespace App\Models;
 
+use Spatie\Activitylog\LogOptions;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
 
 class Companies extends Model
 {
-
-    protected static $logAttributes = ['name', 'email', 'logo', 'website'];
-
-    protected static $logOnlyDirty = true;
-
-    protected static $logName = 'companies';
-
-    public function getDescriptionForEvent(string $eventName): string
-    {
-        return "model companies successfully {$eventName}";
-    }
-
+    use LogsActivity;
+    
     protected $table = "companies";
     protected $primaryKey = 'id';
     protected $fillable = ['id', 'name', 'email', 'logo', 'website'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->useLogName('Companies')
+            ->logOnly(['name', 'email', 'logo', 'website'])
+            ->logOnlyDirty()
+            ->setDescriptionForEvent(fn(string $eventName) => "Model Companies has been {$eventName}")
+            ->dontSubmitEmptyLogs()
+        ;
+    }
 }
