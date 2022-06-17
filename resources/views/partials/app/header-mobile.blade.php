01d445d65af709750c9d28db8fb95a4ea7567a15
Apri Pandu <sarahpalastrin@gmail.com>
2022-06-18 00:14:54 +0700

    Update hamburger menu

--- resources/views/partials/app/header-mobile.blade.php
+++ resources/views/partials/app/header-mobile.blade.php
@@ -16,11 +16,44 @@
     <nav class="navbar-mobile">
         <div class="container-fluid">
             <ul class="navbar-mobile__list list-unstyled">
-                <li>
+                <li class="@if (Request::segment(1) == '') active bg-gradient-primary @endif">
                     <a href="{{ route('home') }}">
-                    <i class="fas fa-chart-line"></i>
-                        Data Dashboard
+                        <i class="fas fa-chart-line"></i>
+                        Dashboard
                     </a>
+                </li>
+                <li class="has-sub @if (Request::segment(1) == 'company') active bg-gradient-primary @endif">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>
+                        Company
+                    </a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li class="@if (Request::segment(2) == 'index') active bg-gradient-primary @endif">
+                            <a href="{{ route('company.index') }}">Index</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="has-sub @if (Request::segment(1) == 'employee') active bg-gradient-primary @endif">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>
+                        Employee
+                    </a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li class="@if (Request::segment(2) == 'index') active bg-gradient-primary @endif">
+                            <a href="{{ route('employee.index') }}">Index</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="has-sub @if (Request::segment(1) == 'system') active bg-gradient-primary @endif">
+                    <a class="js-arrow" href="#">
+                        <i class="fas fa-tachometer-alt"></i>
+                        System
+                    </a>
+                    <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
+                        <li class="@if (Request::segment(2) == 'activity') active bg-gradient-primary @endif">
+                            <a href="{{ route('activity.index') }}">Audit Log</a>
+                        </li>
+                    </ul>
                 </li>
             </ul>
         </div>
